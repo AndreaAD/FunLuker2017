bbd209a6363fc191eda01c7f7ae24d0e0f2d3aa7
Unknown <jonatancastro1@gmail.com>
2017-03-24 07:03:02 -0500

    Cambios paginador

--- es/equipo.php
+++ es/equipo.php
@@ -20,7 +20,8 @@
                 <div class="col-sm-5 col-izq">
                 </div>
                 <div class="col-sm-8 fondo-blanco texto">
-                    <h4 class="subtitulo_texto azul">Equipo <br> <strong>de trabajo</strong></h4>
+                    <h4 class="subtitulo_texto azul">Equipo <br>
+                        <strong>de trabajo</strong></h4>
                     <h5 class="azul destacado">Junta Directiva</h5>
                     <ul class="sinMargin listaAzul">
                         <li>José Alfredo Restrepo Echeverri</li>
@@ -56,29 +57,21 @@
                 <div class="col-sm-5 col-izq">
                 </div>
                 <div class="col-sm-8 fondo-blanco texto">
-                    </br></br></br>
+                    </br>
                     <h5 class="azul destacado">Equipo de trabajo</h5>
                     <h6 class="nombres">Pablo Jaramillo Villegas<br><small>Gerente</small> </h6>
                     <div class="row">
                         <div class="col-sm-8">
-                            <h6 class="nombres">Santiago Isaza Arango <br><small>Director de Educación</small> </h6>
-                            <h6 class="nombres">Ángela Cecilia Vásquez Escobar <br><small>Directora de Estrategia y Proyectos Especiales</small> </h6>
-                            <h6 class="nombres">Constanza Orrego Gómez <br><small>Directora Jurídico Administrativa</small> </h6>
-                            <h6 class="nombres">Juliana Dávila Rincón <br><small>Líder de Comunicaciones</small> </h6>
-                            <h6 class="nombres">Victoria Eugenia Jiménez Castañeda <br><small>Secretaria de Gerencia</small> </h6>
-                            <h6 class="nombres">María Camila Arango Isaza <br><small>Profesional de Educación</small> </h6>
-                            <h6 class="nombres">Ángela María Trujillo Gutiérrez <br><small>Profesional de Educación</small> </h6>
-                            <h6 class="nombres">Andrea Catalina Piedrahita Meza <br><small>Profesional Administrativa</small> </h6>                            
+                            <h6 class="nombres">Santiago Isaza Arango <br><small>Director de Educación</small></h6>
+                            <h6 class="nombres">Ángela Cecilia Vásquez Escobar <br><small>Directora de Estrategia y Proyectos Especiales</small></h6>
+                            <h6 class="nombres">Constanza Orrego Gómez <br><small>Directora Jurídico Administrativa</small></h6>
+                            <h6 class="nombres">Juliana Dávila Rincón <br><small>Líder de Comunicaciones</small></h6>                                                      
                         </div>
                         <div class="col-sm-8">
-                            <h6 class="nombres">Victoria Eugenia Tamayo Lozano <br><small>Líder de Contabilidad</small> </h6>
-                            <h6 class="nombres">Daniela Moreno Gordon <br><small>Asistente Administrativa y de Comunicaciones</small> </h6>
-                            <h6 class="nombres">Andrea Fernanda Muñoz Cuéllar <br><small>Asistente de Proyectos</small> </h6>
-                            <h6 class="nombres">Claudia Milena Rodríguez Valencia<br><small>Analista Contable</small> </h6>
-                            <h6 class="nombres">Fabián Camilo Gallego Gutiérrez  <br><small>Asistente de Cooperación Internacional</small> </h6>
-                            <h6 class="nombres">José Didier Ramírez Rincón <br><small>Auxiliar Proyectos de Educación</small> </h6>                            
-                            <h6 class="nombres">Olga Beatriz Pachón Jiménez <br><small>Servicios Generales</small> </h6>
-                            <h6 class="nombres">Hernando Llano Dávila <br><small>Mensajero</small></h6>
+                            <h6 class="nombres">Victoria Eugenia Jiménez Castañeda <br><small>Secretaria de Gerencia</small></h6>
+                            <h6 class="nombres">María Camila Arango Isaza <br><small>Profesional de Educación</small> </h6>
+                            <h6 class="nombres">Ángela María Trujillo Gutiérrez <br><small>Profesional de Educación</small></h6>
+                            <h6 class="nombres">Andrea Catalina Piedrahita Meza <br><small>Profesional Administrativa</small></h6>  
                         </div>
                     </div>
                     <div class="paginacion pt-trigger-container">
@@ -86,12 +79,12 @@
                           <ul class="pager">
                             <li><a href="#" class="pt-trigger " data-animation="17" data-goto="1">1</a></li>
                             <li><a href="#" class="pt-trigger active" data-animation="17" data-goto="2">2</a></li>
-                            <li><a href="#" class="pt-trigger" data-animation="17" data-goto="3">3</a></li>
-                           
+                            <li><a href="#" class="pt-trigger" data-animation="17" data-goto="3">3</a></li>                           
                           </ul>
                         </nav>
                     </div>
-                    
+                    </br>
+                    <img src="img/EquipoTrabajo.jpg" class="hidden-xs" alt="Equipo de trabajo">
                 </div>
                 <div class="col-sm-3 col-izq">
                     
@@ -102,45 +95,42 @@
             <div class="row contenido">
                 <div class="col-sm-5 col-izq">
                 </div>
-                <div class="col-sm-8 fondo-blanco texto2">
+                
+                <div class="col-sm-8 fondo-blanco texto">
+                    </br></br></br>
                     <div class="row">
                         <div class="col-sm-8">
-                            </br></br></br></br>
-                            <h6 class="nombres">Ángela Cecilia Vásquez Escobar<br>
-                                 Juliana Dávila Rincón<br>
-                                Daniela Moreno Gordon<br>
-                                <small>Coordinación general</small> </h6>
-                            <h6 class="nombres">Bernardo González<br><small>Edición</small> </h6>
-                            <h6 class="nombres"><a class="link_textos" href="http://azoma.net" target="_blank">Azoma.net</a> - <a class="link_textos" href="http://giro360.co" target="_blank">giro360.co</a><br><small>Diseño</small> </h6>
-                            
+                            <h6 class="nombres">Santiago Isaza Arango <br><small>Director de Educación</small> </h6>
+                            <h6 class="nombres">Ángela Cecilia Vásquez Escobar <br><small>Directora de Estrategia y Proyectos Especiales</small> </h6>
+                            <h6 class="nombres">Constanza Orrego Gómez <br><small>Directora Jurídico Administrativa</small> </h6>
+                            <h6 class="nombres">Juliana Dávila Rincón <br><small>Líder de Comunicaciones</small> </h6>
+                            <h6 class="nombres">Victoria Eugenia Jiménez Castañeda <br><small>Secretaria de Gerencia</small> </h6>
+                                                       
                         </div>
                         <div class="col-sm-8">
-                            </br></br></br></br>
-                            <h6>Fundación Luker</h6>
-                            <p>
-                                PBX: (57) (6) 875 64 43 <br>
-                                Cra. 23 # 64 B - 33 piso 3 <br>
-                                Manizales, Caldas <br>
-                                <p><a class="link_textos" href="//facebook.com/FundacionLuker" target="_blank">fb/FundacionLuker</a> <br>
-                                    <a class="link_textos" href="//twitter.com/FundacionLuker" target="_blank">twitter/@FundacionLuker</a><br>
-                                    <a class="link_textos" href="//instagram.com/Funluker" target="_blank">instagram/@Funluker</a><br>
-                                <a class="link_textos" href="mailto:fundacionluker@casaluker.com.co" target="_blank">fundacionluker@casaluker.com.co</a> <br> 
-                                <a class="link_textos" href="//www.fundacionluker.org.co" target="_blank">www.fundacionluker.org.co</a> <br></p>
-                            </p>
+                            <h6 class="nombres">María Camila Arango Isaza <br><small>Profesional de Educación</small> </h6>
+                            <h6 class="nombres">Ángela María Trujillo Gutiérrez <br><small>Profesional de Educación</small> </h6>
+                            <h6 class="nombres">Andrea Catalina Piedrahita Meza <br><small>Profesional Administrativa</small> </h6> 
+                            <h6 class="nombres">Ángela Cecilia Vásquez Escobar<br>
+                            Juliana Dávila Rincón<br>
+                            Daniela Moreno Gordon<br>
+                            <small>Coordinación General Informe de Sostenibilidad 2016</small></h6>
                         </div>
                     </div>
                     <div class="paginacion pt-trigger-container">
                         <nav>
                           <ul class="pager">
                             <li><a href="#" class="pt-trigger " data-animation="17" data-goto="1">1</a></li>
-                            <li><a href="#" class="pt-trigger " data-animation="17" data-goto="2">2</a></li>
+                            <li><a href="#" class="pt-trigger" data-animation="17" data-goto="2">2</a></li>
                             <li><a href="#" class="pt-trigger active" data-animation="17" data-goto="3">3</a></li>
                            
                           </ul>
                         </nav>
                     </div>
-                    
+                    </br>
+                    <img src="img/EquipoTrabajo.jpg" class="hidden-xs" alt="Equipo de trabajo">
                 </div>
+
                 <div class="col-sm-3 col-izq">
                     
                 </div>
